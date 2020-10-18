ea244422d7f6c04455bbd8039d8b56c31c6192e3
yue-cherry-ying <cherryying@uchicago.edu>
2020-10-18 02:27:35 +0000

    finished with assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,10 +9,6 @@ class ApplicationController < ActionController::Base
   end
 
   def play_rock
-    # write a ton of code
-    # redirect_to("https://www.wikipedia.org")
-    # render({ :plain => "Howdy, world!"})
-    # render({ :html => "<h1>Hello, world!</h1>".html_safe})
     render({ :template => "game_templates/user_rock.html.erb"})
   end
 
@@ -28,5 +24,19 @@ class ApplicationController < ActionController::Base
     end
 
     render({ :template => "game_templates/user_paper.html.erb"})
+  end
+
+  def play_scissors
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "lost"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    elsif @comp_move == "scissors"
+      @outcome = "tied"
+    end
+
+    render({ :template => "game_templates/user_scissors.html.erb"})
   end
 end
